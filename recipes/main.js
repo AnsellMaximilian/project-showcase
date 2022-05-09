a6c07707917e911f9c571bd756226fc062594170
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-09 13:43:44 +0700

    Create basic getRecipe function

--- recipes/main.js
+++ recipes/main.js
@@ -2,3 +2,21 @@ const title = document.getElementById("title");
 const finishedImageSection = document.getElementById("finished-image");
 const ingredientsSection = document.getElementById("ingredients");
 const stepsSection = document.getElementById("steps");
+
+const getRecipe = async () => {
+  try {
+    const {
+      data: {
+        recipes: [recipe],
+      },
+    } = await axios.get(
+      "https://api.spoonacular.com/recipes/random?apiKey=1fbea3a03746450ba412eafa8dd62ab6"
+    );
+
+    console.log(recipe);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getRecipe();
