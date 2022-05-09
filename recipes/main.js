e7dec1ae4319865aeac6e052af8cabb5b169ee6a
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-09 13:57:04 +0700

    Remove ol tag and add parent container instead

--- recipes/main.js
+++ recipes/main.js
@@ -1,6 +1,13 @@
 const title = document.getElementById("title");
+
+// Finished Image
 const finishedImageSection = document.getElementById("finished-image");
+const finishedImage = finishedImageSection.querySelector("img");
+
+// Ingredients Section
 const ingredientsSection = document.getElementById("ingredients");
+
+// Steps Section
 const stepsSection = document.getElementById("steps");
 
 const getRecipe = async () => {
@@ -20,8 +27,15 @@ const getRecipe = async () => {
 };
 
 const displayRandomRecipe = async () => {
-  const { image, instructions, extendedIngredients } = await getRecipe();
-  console.log({ image, instructions, extendedIngredients });
+  const {
+    image,
+    instructions,
+    extendedIngredients,
+    title: recipeTitle,
+  } = await getRecipe();
+  title.innerText = recipeTitle;
+  finishedImage.src = image;
+  finishedImage.alt = recipeTitle;
 };
 
 displayRandomRecipe();
