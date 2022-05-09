f9dcddabc03d574453dbe31a5380b593bdcb1176
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-09 13:37:47 +0700

    Create main section DOM references

---
+++ recipes/main.js
@@ -0,0 +1,4 @@
+const title = document.getElementById("title");
+const finishedImageSection = document.getElementById("finished-image");
+const ingredientsSection = document.getElementById("ingredients");
+const stepsSection = document.getElementById("steps");
