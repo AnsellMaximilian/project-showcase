e38193d354f79fbb2a59e3ed0eb0b945d530fc1e
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-14 10:41:14 +0700

    EAS: Refine grid creation logic and add styles

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -1,6 +1,6 @@
 const grid = document.querySelector("#grid");
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 16 * 16; i++) {
   const tile = document.createElement("div");
   tile.classList.add("tile");
   grid.append(tile);
