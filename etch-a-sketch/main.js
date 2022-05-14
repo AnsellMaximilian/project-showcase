3efc203f7b3dabb62ec674988d6e9bffd9568f53
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-14 11:01:18 +0700

    EAS: add change grid size feature
    Changing of grid size just involves rebuilding the grid with the new
    size from the user and updating one root variable in the css. This way,
    dependency of tiles are aoutomatically handled by css's calc function.

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -1,10 +1,27 @@
+const root = document.querySelector(":root");
+
 const grid = document.querySelector("#grid");
 
-for (let i = 1; i <= 16 * 16; i++) {
-  const tile = document.createElement("div");
-  tile.classList.add("tile");
-  tile.addEventListener("mouseover", (e) =>
-    e.target.classList.add("tile--painted")
-  );
-  grid.append(tile);
-}
+// Grid size prompt
+const gridSizeUpdateBtn = document.querySelector("#grid-size-prompt button");
+const gridSizeInput = document.querySelector("#grid-size-input");
+
+gridSizeUpdateBtn.addEventListener("click", () => {
+  const newSize = parseInt(gridSizeInput.value);
+  root.style.setProperty("--grid-dimensions", newSize);
+  buildGrid(newSize);
+});
+
+const buildGrid = (size) => {
+  grid.innerHTML = "";
+  for (let i = 1; i <= size * size; i++) {
+    const tile = document.createElement("div");
+    tile.classList.add("tile");
+    tile.addEventListener("mouseover", (e) =>
+      e.target.classList.add("tile--painted")
+    );
+    grid.append(tile);
+  }
+};
+
+buildGrid(16);
