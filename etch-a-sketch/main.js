2b16aa2b01cb06a991e7fb5358dc4b5a94eda16a
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-14 10:45:43 +0700

    EAS: add hover listener to paint tiles

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -3,5 +3,8 @@ const grid = document.querySelector("#grid");
 for (let i = 1; i <= 16 * 16; i++) {
   const tile = document.createElement("div");
   tile.classList.add("tile");
+  tile.addEventListener("mouseover", (e) =>
+    e.target.classList.add("tile--painted")
+  );
   grid.append(tile);
 }
