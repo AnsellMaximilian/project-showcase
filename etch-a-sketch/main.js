344534fca277b944d78ba28f8e0d60bfac3621fa
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-14 10:33:26 +0700

    EAS: Add basic grid creation logic and include js file in index.html

---
+++ etch-a-sketch/main.js
@@ -0,0 +1,7 @@
+const grid = document.querySelector("#grid");
+
+for (let i = 1; i <= 16; i++) {
+  const tile = document.createElement("div");
+  tile.classList.add("tile");
+  grid.append(tile);
+}
