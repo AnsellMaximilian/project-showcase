6ee4b1bfdf79f5e180c14e83676c8f1acaaab3e4
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-21 13:22:01 +0700

    RCP: Add basic styles, include css, comment js

--- recipes/main.js
+++ recipes/main.js
@@ -1,55 +1,55 @@
-const title = document.getElementById("title");
+// const title = document.getElementById("title");
 
-// Preview Section
-const previewSection = document.getElementById("preview");
-const previewImage = previewSection.querySelector("img");
-const previewDescription = previewSection.querySelector("h2");
-const previewSummary = previewSection.querySelector("p");
+// // Preview Section
+// const previewSection = document.getElementById("preview");
+// const previewImage = previewSection.querySelector("img");
+// const previewDescription = previewSection.querySelector("h2");
+// const previewSummary = previewSection.querySelector("p");
 
-// Ingredients Section
-const ingredientsSection = document.getElementById("ingredients");
-const ingredientList = ingredientsSection.querySelector("ul");
+// // Ingredients Section
+// const ingredientsSection = document.getElementById("ingredients");
+// const ingredientList = ingredientsSection.querySelector("ul");
 
-// Steps Section
-const stepsSection = document.getElementById("steps");
-const stepsListContainer = stepsSection.querySelector("#steps-list-container");
+// // Steps Section
+// const stepsSection = document.getElementById("steps");
+// const stepsListContainer = stepsSection.querySelector("#steps-list-container");
 
-const getRecipe = async () => {
-  try {
-    const {
-      data: {
-        recipes: [recipe],
-      },
-    } = await axios.get(
-      "https://api.spoonacular.com/recipes/random?apiKey=1fbea3a03746450ba412eafa8dd62ab6"
-    );
+// const getRecipe = async () => {
+//   try {
+//     const {
+//       data: {
+//         recipes: [recipe],
+//       },
+//     } = await axios.get(
+//       "https://api.spoonacular.com/recipes/random?apiKey=1fbea3a03746450ba412eafa8dd62ab6"
+//     );
 
-    return recipe;
-  } catch (error) {
-    console.log(error);
-  }
-};
+//     return recipe;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-const displayRandomRecipe = async () => {
-  const {
-    image,
-    instructions,
-    extendedIngredients,
-    title: recipeTitle,
-    summary,
-  } = await getRecipe();
-  title.innerText = recipeTitle;
-  previewImage.src = image;
-  previewImage.alt = recipeTitle;
-  // previewDescription.innerText = "Finished - " + recipeTitle;
-  previewSummary.innerHTML = summary;
-  stepsListContainer.innerHTML = instructions;
-  extendedIngredients.forEach((ingredient) => {
-    const { name, amount, unit } = ingredient;
-    const ingredientListItem = document.createElement("li");
-    ingredientListItem.innerText = name + " (" + amount + " " + unit + ")";
-    ingredientList.appendChild(ingredientListItem);
-  });
-};
+// const displayRandomRecipe = async () => {
+//   const {
+//     image,
+//     instructions,
+//     extendedIngredients,
+//     title: recipeTitle,
+//     summary,
+//   } = await getRecipe();
+//   title.innerText = recipeTitle;
+//   previewImage.src = image;
+//   previewImage.alt = recipeTitle;
+//   // previewDescription.innerText = "Finished - " + recipeTitle;
+//   previewSummary.innerHTML = summary;
+//   stepsListContainer.innerHTML = instructions;
+//   extendedIngredients.forEach((ingredient) => {
+//     const { name, amount, unit } = ingredient;
+//     const ingredientListItem = document.createElement("li");
+//     ingredientListItem.innerText = name + " (" + amount + " " + unit + ")";
+//     ingredientList.appendChild(ingredientListItem);
+//   });
+// };
 
-displayRandomRecipe();
+// displayRandomRecipe();
